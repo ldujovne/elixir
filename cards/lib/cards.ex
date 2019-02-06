03cf438a6c7156a3924d514d9adcbd18bb13a4b8
Luis Dujovne <luisdujovne@MacBook-Pro-de-Luis-2.local>
2019-02-06 09:07:45 -0300

    initial elixir training

---
+++ cards/lib/cards.ex
@@ -0,0 +1,56 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for Cards.
+  """
+
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Club", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"  
+    end  
+    
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)  
+  end
+
+  @doc """
+
+  ## Examples
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+  """
+
+  def contains?(deck, hand) do
+    Enum.member?(deck,hand)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+
+    case File.read(filename) do
+      {:ok,binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "That file that not exists"
+    end
+    
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+  end
+
+end
